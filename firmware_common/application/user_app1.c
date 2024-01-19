c1cf85364d0f59948584c61aaa548906adbad15e
jennbushey <117766897+jennbushey@users.noreply.github.com>
2024-01-18 20:35:49 -0700

    started debug module

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -54,6 +54,8 @@ extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.
 extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
 extern volatile u32 G_u32ApplicationFlags;                /*!< @brief From main.c */
 
+extern u8 G_au8DebugScanfBUffer[];
+extern u8 G_u8DebugScanfCharCount;
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
@@ -61,7 +63,8 @@ Variable names shall start with "UserApp1_<type>" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_pfStateMachine;               /*!< @brief The state machine function pointer */
 //static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout counter used across states */
-
+// ledpwm(5) // turns on the LED but dimmer
+static u8 au8UserInputBuffer[USER1_INPUT_BUFFER_SIZE]; /* char buffer */
 
 /**********************************************************************************************************************
 Function Definitions
@@ -92,6 +95,22 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  u8 u8String[] = "A string to print that returns cursor to start of next line. \n\r";
+  u8 u8String2[] = "Here's a number ";
+  u8 u8String3[] = " The 'cursor' was here.";
+  u32 u32Number = 1234567;
+  
+  DebugPrintf(u8String);
+  DebugPrintf(u8String2);
+  DebugPrintNumber(u32Number);
+  DebugPrintf(u8String3);
+  DebugLineFeed();
+  DebugPrintf(u8String3);
+  DebugLineFeed();
+  
+  for(u8 i = 0; i < USER1_INPUT_BUFFER_SIZE; i++){
+    au8UserInputBuffer[i] = 0;
+  }
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,7 +159,32 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
+  static u8 u8NumCharsMessage[] = "\n\rCharacters in buffer: ";
+  static u8 u8BufferMessage[] = "\n\rBuffer contents:\n\r";
+  u8 u8CharCount;
+  
+  /* Print message with number of characters in scanf buffer*/
+#if 0
+  if(WasButtonPressed(BUTTON0)){
+  ButtonAcknowledge(BUTTON0);
+  DebugPrintf(u8NumCharsMessage);
+  DebugPrintNumber(G_u8DebugScanfCharCount);
+  DebugLineFeed();
+  }
+#endif
+  
+  if(WasButtonPressed(BUTTON1)){
+    ButtonAcknowledge(BUTTON1);
     
+    /* Read the buffer */
+    u8CharCount = DebugScanf(au8UserInputBuffer);
+    au8UserInputBuffer[u8CharCount] = '\0';
+    
+    /*Print the heading message and then print the buffer*/
+    DebugPrintf(u8BufferMessage);
+    DebugPrintf(au8UserInputBuffer);
+    DebugLineFeed();
+  }
 } /* end UserApp1SM_Idle() */
      
 
